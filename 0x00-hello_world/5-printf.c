7514b2af1dcf234ecb7b51181c42efae8c6b1658
zain <eduzain1@gmail.com>
2022-09-16 08:09:23 -0700

    hi

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,12 +8,12 @@
 /**
  * main - print:wqs "with proper grammar, but the outcome is
  *                a piece of art," followed by a new line.
- * 
- * Return: Always 0.
+ *
+ * Return:Always 0.
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-        return(0);
-}	
+return (0);
+}
