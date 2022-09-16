8326ea41d795c61db73993ec1664208618dee585
zain <eduzain1@gmail.com>
2022-09-16 07:15:55 -0700

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,19 @@
 /*
- *  * File: 5-printf.c
- *   * Auth: Brennan D Baraban
- *    */
+ * File: 5-printf.c
+ * Auth: Brennan D Baraban
+ */
 
 #include <stdio.h>
 
 /**
- * main - Prints "with proper grammar, but the outcome is
- *                 a piece of art," followed by a new line.
+ * main - printf "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
  * 
  * Return: Always 0.
  */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+        printf("with proper grammar, but the outcome is a piece of art,\n");
 
-		return (0);
-}
+        return(0);
+}	
