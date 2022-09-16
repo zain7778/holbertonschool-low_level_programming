06b89b160df5b715adbca796ee5f525442262b93
zain <eduzain1@gmail.com>
2022-09-16 10:31:24 -0700

    hi

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,13 @@
  */
 int main(void)
 {
-char letter;
+	char letter;
+
 for (letter = 'a'; letter <= 'z'; letter++)
 {
-if (letter != 'e' && letter != 'q')							putchar(letter);
+	if (letter != 'e' && letter != 'q)
+		putchar(letter);
 }
-putchar('\n');
-return (0);
+		putchar('\n');
+		return (0);
 }
