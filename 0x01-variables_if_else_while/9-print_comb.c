d29b399376dc560d5cf6a9703715481ef1df940d
zain <eduzain1@gmail.com>
2022-09-19 08:49:35 -0700

    hello

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,18 +12,16 @@
  */
 int main(void)
 {
-		int num;
+	int num;
 
-			for (num = 0; num <= 9; num++)
-					{
-putchar((num % 10) + '0');
-
-if (num == 9)														continue;												
-												putchar(',');
-														putchar(' ');
-															}
-
-				putchar('\n');
-
-					return (0);
+	for (num = 0; num <= 9; num++)
+	{
+	putchar((num % 10) + '0');
+	if (num == 9)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}	
+	putchar('\n');
+	return (0);
 }
