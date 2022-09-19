22d16fbbe97384844738f144fd351715a9726870
zain <eduzain1@gmail.com>
2022-09-19 08:25:50 -0700

    hi

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,26 +1,30 @@
 /*
- * File: 9-print_comb.c
- * Auth: Brennan D Baraban
- */
+ *  * File: 9-print_comb.c
+ *   * Auth: Brennan D Baraban
+ *    */
 
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of single-digit numbers.
- *
- * Return: Always 0.
- *
-	int main(void)
-	{											int num;																				
-	for (num = 0; num <= 9; num++)
-	{											putcher((num % 10) + '0');
-	if (num == 9)										continue;
+ *  * main - Prints all possible combinations of single-digit numbers.
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+		int num;
 
-	putcher(',');
-	putcher(' ');
-	}
+			for (num = 0; num <= 9; num++)
+					{
+								putchar((num % 10) + '0');
+										if (num == 9)
+														continue;
+												
+												putchar(',');
+														putchar(' ');
+															}
 
-	putcher('\n');
+				putchar('\n');
 
-	return (0);
+					return (0);
 }
