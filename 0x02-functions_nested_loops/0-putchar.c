dc73c3f868b851a5b17865bd4ec1057d7ebc46ec
zain <eduzain1@gmail.com>
2022-09-20 08:35:38 -0700

    hi

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 int main(void)
 {
--putchar('_')
--putchar('p')
--putchar('u')
--putchar('t')
--putchar('c')
--putchar('h')
--putchar('a')
--putchar('r')
+-putchar('_');
+-putchar('p');
+-putchar('u');
+-putchar('t');
+-putchar('c');
+-putchar('h');
+-putchar('a');
+-putchar('r');
 	return(0);
 }
