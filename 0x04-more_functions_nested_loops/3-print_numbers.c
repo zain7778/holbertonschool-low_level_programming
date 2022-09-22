8222586a7a2962b562626a5ead940751ecb0092d
zain <eduzain1@gmail.com>
2022-09-22 11:24:08 -0700

    hi

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,17 @@
 /*
-    * File: 3-print_numbers.c
-     * Auth: Brennan D Baraban
-      */
-
+ * File: 3-print_numbers.c
+ * Auth: Brennan D Baraban
+ */
 #include "main.h"
-
 /**
-   * print_numbers - Prints the numbers from 0-9.
-    */
+ * print_numbers - Prints the numbers from 0-9.
+ */
 void print_numbers(void)
 {
-		int num;
+	int num;
 
-			for (num = 0; num <= 9; num++)
-						_putchar((num % 10) + '0');
+	for (num = 0; num <= 9; num++)
 
-				_putchar('\n');
+		_putchar((num % 10) + '0');
+	_putchar('\n');
 }
