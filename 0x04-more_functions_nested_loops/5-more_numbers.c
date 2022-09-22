5b0e413a1841e19e34fe0b73adae4d1ec2d9afb6
zain <eduzain1@gmail.com>
2022-09-22 11:58:56 -0700

    hi

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,15 +1,17 @@
 /*
- * File: 5-more_numbers.c
- * Auth: Brennan D Baraban
- */
-#include "holberton.h"
+    * File: 5-more_numbers.c
+     * Auth: Brennan D Baraban
+      */
+
+#include "main.h"
+
 /**
- * more_numbers - Prints the numbers 0-14 ten times.
- */
+   * more_numbers - Prints the numbers 0-14 ten times.
+    */
 void more_numbers(void)
 {
-
 	int num, count;
+
 	for (count = 0; count <= 9; count++)
 	{
 		for (num = 0; num <= 14; num++)
@@ -18,6 +20,6 @@ void more_numbers(void)
 				_putchar((num / 10) + '0');
 			_putchar((num % 10) + '0');
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
